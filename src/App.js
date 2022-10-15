0a93dfdf826498cb5469002994dc8e82619fd241
Claudio <claudiobozzotta@gmail.com>
2022-10-15 10:40:54 +0200

    exercise completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+const algoritmo = (array) => {
+
+    let result= 1;
+    let sum= 0;
+
+    for (let i=0; i < array.length; i++) {
+        result = result * array[i];
+    }
+    if(result%2==0){
+      console.log("Sì, pari!" + result);
+      for (let i=0; i < array.length; i++) {
+        sum = sum + array[i];
+      }
+    } else {
+      sum = "non pervenuta perché il prodotto è dispari"
+    }
+    
+    return sum;
+
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Vediamo
+      <h1>
+        {algoritmo([1,2,1,2,8])}
+      </h1>
+      <h1>
+        {algoritmo([8,9,9,9,36,45,5,7,2,7,2,1])}
+      </h1>
+      <h1>
+        {algoritmo([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])}
+      </h1>
     </div>
   );
 }
